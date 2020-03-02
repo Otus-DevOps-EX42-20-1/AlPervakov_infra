eee6e7a7aa35849c40dfbc947e1483aa8a2dd83f
Alexey Pervakov <alpervakov@gmail.com>
2020-03-02 16:37:32 +0300

    HW 6

---
+++ terraform/variables.tf
@@ -0,0 +1,22 @@
+variable project {
+  description = "Project ID"
+}
+variable region {
+  description = "Region"
+  # Значение по умолчанию
+  default = "europe-west1"
+}
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+variable disk_image {
+  description = "Disk image"
+}
+
+variable zone {
+  description = "Zona"
+  default = "europe-west1-b"
+}
+
+variable private_key {}
