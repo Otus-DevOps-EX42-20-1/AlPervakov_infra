4f7ea54f140587d70b6cb9cbdb77ebfaf870e5a0
Alexey Pervakov <alpervakov@gmail.com>
2020-03-03 18:24:41 +0300

    Added HW 7

---
+++ terraform/modules/app/variables.tf
@@ -0,0 +1,14 @@
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+
+variable zone {
+  description = "Zona"
+  default     = "europe-west1-b"
+}
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "app-base"
+}
